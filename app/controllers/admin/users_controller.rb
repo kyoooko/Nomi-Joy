2910136cae2d3a4e102b187a07f820ed8b253ffc
飯田恭子 <iidakyouko@mba.local>
2020-08-05 23:40:52 +0900

    [fix]画像表示をS3を連携(admin側）

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,7 @@
 class Admin::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    # 参照先のS3オブジェクトURLを作成
+    @image_url = "https://dmm-cloud-lesson10-image-files-resize.s3-ap-northeast-1.amazonaws.com/store/" + @user.image_id + "-thumbnail."
   end
 end
