bb93dab22ace72be60a3e48393cc23a516b1aa7b
飯田恭子 <iidakyouko@mba.local>
2020-08-18 00:23:04 +0900

    [fix]wheneverで未読通知3件以上のユーザーにメール送信(Capistrano）

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,10 @@ set :rbenv_ruby, '2.5.7'
 set :linked_files, %w(config/master.key .env)
 append :linked_dirs, "log", "public/system", "tmp"
 
+# バッジ（定時）処理
+set :whenever_roles,        ->{ :app }
+# set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+# set :whenever_roles,        ->{ :batch }
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
